f5d1bde4ddfc69e81265ea0f76f0d5d6a8cf7b2b
Robert Peters <peters.robert.j@gmail.com>
2017-05-24 09:46:14 -0400

    Added CORS middleware

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Additional providers
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
